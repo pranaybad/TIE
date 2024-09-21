f45ce487055d0362f58dadd600b42fd86133af45
pranaygit <pranaybadgujar123@gmail.com>
2024-09-21 15:01:16 +0530

    about add

--- tie/baseapp/urls.py
+++ tie/baseapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import  include
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('jobopenings', include('jobopenings.urls')),
+    path('learningapp', include('learningapp.urls')),
+    path('freelanceapp', include('freelanceapp.urls')),
 ]
